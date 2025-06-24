8d60632861742798152309ef00cb15396d2170d8
Hyteq <DerpsDer.es@gmail.com>
2025-06-24 11:23:40 +0300

    chore(turborepo): initial setup and config, move db to package

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+// Load the right env file based on environment
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else {
+  dotenv.config({ path: ".env.local" });
+}
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export default {
+  schema: "./src/schema.ts",
+  dialect: "postgresql",
+  migrations: {
+    table: "drizzle_migrations",
+  },
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  out: "./migrations",
+  strict: process.env.NODE_ENV === "production",
+} satisfies Config; 
