11d988b422c10f98c5e5d69eff1e00ce2adcfeaa
Maze Winther <mazewinther@gmail.com>
2025-07-17 19:46:23 +0200

    stable timeline

--- apps/web/src/components/editor/snap-indicator.tsx
+++ apps/web/src/components/editor/snap-indicator.tsx
@@ -40,7 +40,7 @@ export function SnapIndicator({
 
   return (
     <div
-      className="absolute pointer-events-none z-[90] ml-48"
+      className="absolute pointer-events-none z-[90]"
       style={{
         left: `${leftPosition}px`,
         top: 0,
